b228da9dbb5b37dd2a11fdb736d55c64460a90d7
Rockyibarra96 <rockyibarralutz@gmail.com>
2024-03-18 16:39:06 -0700

    finished project

--- index.js
+++ index.js
@@ -8,9 +8,17 @@ function newImage(url, left, bottom){
     return object
 }
 
+
 function newItem(url, left, bottom){
     let item = newImage(url, left, bottom)
+    item.addEventListener('click', function(){
+        item.remove();
+        let inventoryItem = document.createElement('img')
+        inventoryItem.src = url
+        inventory.append(inventoryItem)
+    })
 }
+
 
 function newInventory(){
     let inventory = document.createElement('div')
@@ -26,9 +34,12 @@ function newInventory(){
     inventory.style.border = '2px solid black'
     inventory.style.backgroundColor = 'brown'
     document.body.append(inventory)
+    return inventory
 }
 
-newInventory()
+let inventory = newInventory()
+
+
 newImage('assets/green-character.gif', 100, 250)
 newImage('assets/tree.png', 200, 450)
 newImage('assets/pillar.png', 350, 250)
@@ -39,3 +50,5 @@ newImage('assets/well.png', 500, 575)
 newItem('assets/sword.png', 500, 555)
 newItem('assets/shield.png', 165, 335)
 newItem('assets/staff.png', 600, 250)
+
+
